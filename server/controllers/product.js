75f86cf34837bc314189813a0d604abdf5e2d321
Chrugez <thanhcuis99@gmail.com>
2023-12-07 23:21:03 +0700

    #10 Filtering, Sorting & Pagination Product

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const Product = require('../models/product')
 const asyncHandler = require('express-async-handler')
 const slugify = require('slugify')
@@ -22,10 +23,50 @@ const getProduct = asyncHandler(async (req, res) => {
 })
 //Filtering, sorting & pagination
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find()
-    return res.status(200).json({
-        success: products ? true : false,
-        productDatas: products ? products : 'Cannot get products!'
+    const queries = { ...req.query }
+    //tách các trường đặc biệt ra khỏi query
+    const excludeFields = ['limit', 'sort', 'page', 'fields']
+    excludeFields.forEach(el => delete queries[el])
+
+    //format lại các operators cho đúng cú pháp của mongoose
+    let queryString = JSON.stringify(queries)
+    queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, matchEl => `$${matchEl}`)
+    const formatedQueries = JSON.parse(queryString)
+
+    //filtering
+    if (queries?.title) formatedQueries.title = { $regex: queries.title, $options: 'i' }
+    let queryCommand = Product.find(formatedQueries)
+
+    //sorting
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ')
+        queryCommand = queryCommand.sort(sortBy)
+    }
+
+    //fields limiting
+    if (req.query.fields) {
+        const fields = req.query.fields.split(',').join(' ')
+        queryCommand = queryCommand.select(fields)
+    }
+
+    //pagination
+    //limit: số object lấy về 1 lần gọi API
+    //skip: Bỏ qua bao nhiêu object rồi lấy tiếp (tính từ object đầu tiên)
+    const page = +req.query.page || 1
+    const limit = +req.query.limit || process.env.LIMIT_PRODUCTS
+    const skip = (page - 1) * limit
+    queryCommand.skip(skip).limit(limit)
+
+    //execute query
+    //số lượng sản phẩn thỏa mãn điều kiện !== số lượng sản phẩm trả về 1 lần gọi API
+    queryCommand.exec(async (err, response) => {
+        if (err) throw new Error(err.message)
+        const counts = await Product.find(formatedQueries).countDocuments()
+        return res.status(200).json({
+            success: response ? true : false,
+            counts,
+            products: response ? response : 'Cannot get products!',
+        })
     })
 })
 
@@ -48,10 +89,36 @@ const deleteProduct = asyncHandler(async (req, res) => {
     })
 })
 
+const ratings = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const { star, comment, pid } = req.body
+    if (!star || !pid) throw new Error('Missing inputs!')
+    const ratingProduct = await Product.findById(pid)
+    const alreadyRating = ratingProduct?.rating?.find(el => el.postedBy.toString() === _id)
+    if (alreadyRating) {
+        await Product.updateOne({
+            rating: { $elemMatch: alreadyRating }
+        }, {
+            $set: { "rating.$.star": star, "rating.$.comment": comment }
+        }, { new: true })
+    } else {
+        await Product.findByIdAndUpdate(pid, {
+            $push: { rating: { star, comment, postedBy: _id } }
+        }, { new: true })
+    }
+
+    //sum rating
+
+    return res.status(200).json({
+        status: true,
+    })
+})
+
 module.exports = {
     createProduct,
     getProduct,
     getProducts,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    ratings
 }
